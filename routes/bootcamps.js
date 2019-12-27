f883fa6c3e134ef927eb47741d6e20f73b492953
illusionist <nikhil.shrestha1995@gmail.com>
2019-12-27 19:32:04 +0545

    adding express router

---
+++ routes/bootcamps.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+});
+
+router.get('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
+});
+
+router.post('/', (req, res) => {
+  res.status(200).json({ success: true, message: 'Create new bootcamps' });
+});
+
+router.put('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `delete bootcamp ${req.params.id}` });
+});
+
+module.exports = router;
