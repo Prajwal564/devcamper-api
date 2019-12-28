208919e240bc00b518fecfd1a92e82b4883a94ea
illusionist <nikhil.shrestha1995@gmail.com>
2019-12-28 18:52:38 +0545

    user register and encrypt password using bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,5 +7,14 @@ const User = require('../models/User');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(201).json({ success: true, data: {} });
+  const { name, email, password, role } = req.body;
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+  res.status(201).json({ success: true });
 });
