8e04b8c7c4918a8cd7a2c5a69ee7a5a1231ec3c3
illusionist <nikhil.shrestha1995@gmail.com>
2019-12-29 07:27:03 +0545

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,3 +22,36 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, token });
 });
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // check fo user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  console.log(isMatch);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
+});
