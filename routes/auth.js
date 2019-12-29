fb3060ec43dc842d073693f35fbfaa6cc5fba470
illusionist <nikhil.shrestha1995@gmail.com>
2019-12-29 08:06:51 +0545

    auth protect middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const { login, register } = require('../controllers/auth');
+const { login, register, getMe } = require('../controllers/auth');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 router.post('/login', login);
 router.post('/register', register);
+router.get('/me', protect, getMe);
 
 module.exports = router;
